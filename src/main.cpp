888f41765ea041e2937a5f003529534445dff5e9
Earth_Studio <earthstudiomail@gmail.com>
2026-04-27 18:51:53 +0800

    add some docs

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,32 @@
 #include "content/window/BasicWindow/BasicWindow.h"
 
 int main() {
+    // Before you start to write application, you must initialize the Framework first:
+
     cmaterial::Framework app;
     app.initialize();
 
+    // Add your Listener:
     cmaterial::event::EventBus::addListener(new cmaterial::event::WindowLogic(&app));
 
+    // Create your windows:
     cmaterial::window::BasicWindow* window1 = new cmaterial::window::BasicWindow("window1", 300, 100);
     cmaterial::window::BasicWindow* window2 = new cmaterial::window::BasicWindow("window2", 300, 100);
 
+    // Create your components:
     cmaterial::component::BasicButton* button1 = new cmaterial::component::BasicButton();
     button1->name = "buttonInWindow1";
     cmaterial::component::CreatorButton* button2 = new cmaterial::component::CreatorButton();
     button2->name = "buttonInWindow2";
 
+    // To inject components in an exist window:
     app.addWindow(window1);
     window1->addComponent(button1);
 
     app.addWindow(window2);
     window2->addComponent(button2);
 
+    // Finally, run your app by doing this:
     app.run();
     return 0;
 }
